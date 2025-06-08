169d0d5d55a2a44010489fa3d81dc078eef37fd0
NAYUDU RATNA SITARA <sitaranayudu2004@gmail.com>
2025-06-08 14:46:33 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Triangle
+{
+	public static void main(String args[])
+	{
+		int a=2;
+		int b=3;
+		int c=3;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter base:");
+		float base=sc.nextFloat();
+		System.out.println("Enter height:");
+		float height=sc.nextFloat();
+		System.out.println("Area of the triangle:" +0.5*base*height);
+		System.out.println("Perimeter of the triangle:" +a+b+c);
+	}
+}
+
+		
+
